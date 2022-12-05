9648fc8f51481894d5653136a9ddd86ddb51cc12
Brianna Dukes <skylar.deep@yahoo.com>
2022-12-05 00:38:33 -0500

    animepage work

--- src/pages/AnimePage/AnimePage.jsx
+++ src/pages/AnimePage/AnimePage.jsx
@@ -1,11 +1,13 @@
 import { useState, useEffect } from 'react';
+import Pagination from 'react-bootstrap/Pagination'
 
 export default function AnimePage() {
   
 const fetch = require('node-fetch');
 const [data, setData] = useState([])
+const [query, setQuery] = useState([])
 
-const url = 'https://anime-db.p.rapidapi.com/anime?page=1&size=20';
+const url = `https://anime-db.p.rapidapi.com/anime?page=1&size=20&search=+${query}`;
 
 const options = {
   method: 'GET',
@@ -20,18 +22,25 @@ fetch(url, options)
 	.then(json => setData(json.data))
 	.catch(err => console.error('error:' + err));
 
-  
-return (
-  
-          <div>
-            <h1>Anime List</h1>
+  const onChangeHandler = (e) => {
+    setQuery(e.target.value)
+  }
 
-            {data.map(data => (
-              <div key={data._id}>
-                <h5>{data.title}</h5>
-                {/* <h5>{data.image}</h5> */}
-              </div>
-           ))}
-          </div>
+return (
+  <div>
+    <h1>Anime List</h1>
+    <form>
+      <input type="text" value={query} onChange={onChangeHandler}/>
+      <button>Search</button>
+    </form>
+
+    {data.map(data => (
+      <div key={data._id}>
+        <h5>{data.title}</h5>
+        <img src={data.image}/>
+      </div>
+))}
+ 
+    </div>
       );
   }
